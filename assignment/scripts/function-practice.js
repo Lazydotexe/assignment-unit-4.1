0156cbf15103610052229e6e5f4da752d532ca14
LazyDotExe <papaporo@proton.me>
2023-06-26 23:23:13 -0500

    Completed the array problems and also tried the stretch goals.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -75,40 +75,83 @@ console.log(getLast([]));
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find(value, array) {
 
-}
+
+function find(value, array) {
+  
+  for (let i = 0; i < array.length; i++)
+    if (array[i] === value) {
+      return true;
+    } 
+    return false
+  }
+console.log(find("a", [1, 2, 3, "a"]));
+console.log(find("Bob", [1, 2, 3, "a"]));
 
 // ----------------------
 // Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
+
+
 function isFirstLetter(letter, string) {
+  const array = string.split('');//This passes my string to the array and the .split('') make it [s, t, r, i, n, g] inseatd of [string]
+    if (array[0] === letter) {
+      return true;
+    } else {
+      return false;
+    }
+
 
 }
 console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll() {
+function sumAll(arr) {
   let sum = 0;
   // TODO: loop to add items
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum
 
   // TODO: return the sum
 }
+console.log(`The sum of your array is ${sumAll([1, 2, 3])}`);
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-function allPositive() {
+function allPositive(arr) {
+  let newArr = [];
 
+  for (let i = 0; i < arr.length; i ++) {
+    if (arr[i] > 0) {
+        newArr.push(i + 1)
+    } else if (arr <= 0) {
+        newArr = [];
+    }
+  }
+    return newArr
 }
+  console.log(allPositive([1, 2, 3, 4]))
+  console.log(allPositive([-1, -2, -3, -4]))
+  console.log(allPositive([1, -2, 3, 4]))
 
 
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
+  
+ // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+function convert(minutes) { //declares a funtion, sets the function name to 'convert' that takes one perameter.
+	minutes *= 60 // this line will multiply the perameter by 60 and also set itself equil to the outcome.
+	return minutes // this line will return the outcome of the previous line of code
+	
+}
+console.log(convert(2)); //finally you call the function using whatever perameter you have given it.
 
 
 // DO NOT MODIFY
